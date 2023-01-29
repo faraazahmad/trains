f8616189d6a8f55185397c5dba8ed87f969657fc
Syed Faraaz Ahmad <faraaz98@live.com>
2023-01-29 18:44:15 +0530

    add method dto

--- lib/trains/dto/controller.rb
+++ lib/trains/dto/controller.rb
@@ -2,6 +2,6 @@ require 'set'
 
 module Trains
   module DTO
-    Controller = Struct.new('Controller', :name, :methods, keyword_init: true)
+    Controller = Struct.new('Controller', :name, :method_list, keyword_init: true)
   end
 end
